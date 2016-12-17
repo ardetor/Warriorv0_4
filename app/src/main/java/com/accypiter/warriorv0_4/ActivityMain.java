c4fdfb61b87607da90805e20ecf5f65c69ac3c0d
ardetor <ardetor@gmail.com>
2016-12-17 10:31:52 +0800

    Added title text to ActivityMain

--- app/src/main/java/com/accypiter/warriorv0_4/ActivityMain.java
+++ app/src/main/java/com/accypiter/warriorv0_4/ActivityMain.java
@@ -45,6 +45,7 @@ public class ActivityMain extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+
         }
 
         return super.onOptionsItemSelected(item);
