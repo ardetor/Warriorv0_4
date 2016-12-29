9def51905bffea1e8e4f37d36ea9c619de497169
ardetor <ardetor@gmail.com>
2016-12-29 21:53:53 +0800

    ActivityPreferences toolbar added.

--- app/src/main/java/com/accypiter/warriorv0_4/ActivityPreferences.java
+++ app/src/main/java/com/accypiter/warriorv0_4/ActivityPreferences.java
@@ -3,14 +3,40 @@ package com.accypiter.warriorv0_4;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class ActivityPreferences extends PreferenceActivity {
+import static java.security.AccessController.getContext;
+
+public class ActivityPreferences extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+        setContentView(R.layout.activity_preferences);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        getFragmentManager().beginTransaction().replace(R.id.activity_preferences_placeholder, new MyPreferenceFragment()).commit();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity_blank, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == android.R.id.home) {
+            this.finish();
+        }
+        return super.onOptionsItemSelected(menuItem);
     }
 
     public static class MyPreferenceFragment extends PreferenceFragment
